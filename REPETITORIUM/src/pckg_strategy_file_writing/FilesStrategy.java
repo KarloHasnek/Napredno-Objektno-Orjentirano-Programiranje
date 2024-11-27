28caae66c0c3e5dc56f15b8eba9bae2149e2e48d
Karlo-DELL <karlohasnek@gmail.com>
2024-11-27 19:28:38 +0100

    Strategy pattern repetitorium

--- REPETITORIUM/src/pckg_strategy_file_writing/FilesStrategy.java
+++ REPETITORIUM/src/pckg_strategy_file_writing/FilesStrategy.java
@@ -3,6 +3,7 @@ package pckg_strategy_file_writing;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 
 public class FilesStrategy implements WriteStrategy{
@@ -10,7 +11,7 @@ public class FilesStrategy implements WriteStrategy{
     @Override
     public void writeToFile(String content, String filePath) {
         try {
-            Files.write(Paths.get(filePath), content.getBytes());
+            Files.write(Paths.get(filePath), content.getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
